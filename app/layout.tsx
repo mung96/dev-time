cdca9c2b2d890193abf6ee6faa6720a693113594
mung96 <mung5079@gmail.com>
2025-12-04 20:17:05 +0900

    refactor: ReactQueryProvider app폴더로 이동

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Providers from "@/app/providers";
+import { ReactQueryProvider } from "@/src/app";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={`${geistSans.variable} ${geistMono.variable} `}>
-        <Providers>{children}</Providers>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
     </html>
   );
