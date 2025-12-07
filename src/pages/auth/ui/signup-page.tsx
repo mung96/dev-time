33f5b942a48c8aee0a340e985159bc97a73102bc
송현명 <mung5079@gmail.com>
2025-12-07 19:46:03 +0900

    DevTime-Week1 (#5)
    * 개발 환경 세팅 (#1)
    
    * 폴더 구조 세팅 (#2)
    
    * chore: FSD 폴더구조 세팅
    
    * refactor: ReactQueryProvider app폴더로 이동
    
    * refactor: globals.css app으로 이동
    
    * feat: tsconfig에 @경로 추가
    
    * 디자인 시스템 구현 (Color, Typography, Button, TextField) (#3)
    
    * design: color 등록 (Light)
    
    * design: Pretendard 추가
    
    * design: Typography 등록
    
    * chore: clsx 라이브러리 추가
    
    * feat: Button 컴포넌트 구현 (primary만)
    
    * feat: helper-text 구현
    
    * feat: text-field 구현
    
    * 회원가입 페이지 퍼블리싱 (#4)
    
    * feat: 회원가입 페이지 라우팅 설정
    
    * fix: 디자인 토큰 오타 수정
    
    * feat: 회원가입 페이지 마크업
    
    * feat: 로그인 페이지 라우팅 설정
    
    * chore: 패키지 name변경
    
    * 회원가입 페이지 유효성검사 (비동기 로직 제외) (#6)
    
    * chore: yarn.lock update
    
    * feat: 회원가입 유효성 검사 로직 구현(비동기 제외)
    
    * feat: 이메일 중복 검사 함수 구현
    
    * refactor: 회원가입 스키마 model로 분리
    
    * chore: sign-up을 signup으로 변경
    
    * chore: button 폴더 케밥케이스로 변경
    
    * refactor: 페이지 url 상수로 분리

---
+++ src/pages/auth/ui/signup-page.tsx
@@ -0,0 +1,142 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { checkEmail } from "@pages/auth/api/check-email";
+import {
+  SignUpFormValues,
+  SignUpFormValuesSchema,
+} from "@pages/auth/model/signup-form-values.schema";
+import { PATH } from "@shared/routes";
+import { Button } from "@shared/ui";
+import { TextField } from "@shared/ui/text-field";
+import { HelperText } from "@shared/ui/text-field/helper-text";
+import { TextFieldButton } from "@shared/ui/text-field/text-field-button";
+import Image from "next/image";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+
+export const SignupPage = () => {
+  const {
+    register,
+    getValues,
+    formState: { errors },
+  } = useForm<SignUpFormValues>({
+    defaultValues: {
+      email: "",
+      nickname: "",
+      password: "",
+      confirmPassword: "",
+    },
+    resolver: zodResolver(SignUpFormValuesSchema),
+    mode: "onTouched",
+  });
+  return (
+    <div className="flex h-full">
+      <div className="flex-1 bg-primary flex items-center justify-center">
+        <div className="flex flex-col gap-9 items-center justify-center">
+          <picture>
+            <Image
+              width={264}
+              height={200}
+              src="/images/Logo.png"
+              alt="DevTime 로고"
+            />
+          </picture>
+          <span className="text-white text-title font-semibold">
+            개발자를 위한 타이머
+          </span>
+        </div>
+      </div>
+      <div className="flex-1 flex justify-center w-full">
+        <div className="flex flex-col items-center gap-6">
+          <form className="mt-[140px] w-105 flex flex-col gap-6">
+            <h2 className="text-heading text-primary text-center">회원가입</h2>
+
+            <fieldset className="flex flex-col gap-10">
+              <TextField
+                label={"아이디"}
+                placeholder="이메일 주소 형식으로 입력해 주세요."
+                {...register("email")}
+                button={
+                  <TextFieldButton
+                    type="button"
+                    onClick={async () => {
+                      const email = getValues("email");
+                      const res = await checkEmail(email);
+                      console.log(res);
+                    }}
+                  >
+                    중복 확인
+                  </TextFieldButton>
+                }
+                helperText={
+                  <HelperText state={"error"}>
+                    {errors.email?.message}
+                  </HelperText>
+                }
+              />
+              <TextField
+                label={"닉네임"}
+                placeholder="닉네임을 입력해 주세요."
+                {...register("nickname")}
+                button={<TextFieldButton>중복 확인</TextFieldButton>}
+                helperText={
+                  <HelperText state={"error"}>
+                    {errors.nickname?.message}
+                  </HelperText>
+                }
+              />
+              <TextField
+                label={"비밀번호"}
+                type="password"
+                {...register("password")}
+                placeholder="비밀번호를 입력해 주세요."
+                helperText={
+                  <HelperText state={"error"}>
+                    {errors.password?.message}
+                  </HelperText>
+                }
+              />
+              <TextField
+                label={"비밀번호 확인"}
+                type="password"
+                {...register("confirmPassword")}
+                placeholder="비밀번호를 다시 입력해 주세요."
+                helperText={
+                  <HelperText state={"error"}>
+                    {errors.confirmPassword?.message}
+                  </HelperText>
+                }
+              />
+              <section>
+                <div className="flex justify-between">
+                  <span>이용약관</span>
+                  <div>체크박스</div>
+                </div>
+                <div className="bg-gray-50 h-[110px] overflow-y-scroll py-3 px-4">
+                  {TERMS_OF_SERVICE_CONTENT}
+                </div>
+              </section>
+            </fieldset>
+
+            <Button priority={"primary"}> 회원가입</Button>
+          </form>
+
+          <p>
+            <span className="text-primary text-body font-medium">
+              회원이신가요?
+            </span>{" "}
+            <Link
+              href={PATH.LOGIN}
+              className="text-primary text-body font-bold "
+            >
+              로그인 바로가기
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const TERMS_OF_SERVICE_CONTENT =
+  "안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕";
