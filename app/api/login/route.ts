72f63439a7364109e09b000a0e7445457f4ffa1a
mung96 <mung5079@gmail.com>
2025-12-09 09:51:03 +0900

    feat: login route 구현

---
+++ app/api/login/route.ts
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const ACCESS_TOKEN_STORAGE_KEY = "accessToken";
+const REFRESH_TOKEN_STORAGE_KEY = "refreshToken";
+const ACCESS_TOKEN_MAX_AGE = 60 * 60; //1시간
+const REFRESH_TOKEN_MAX_AGE = 10 * 24 * 60 * 60; //10일
+
+export async function POST(req: NextRequest) {
+  const payload = await req.json();
+
+  const response = await fetch(`${process.env.API_SERVER_URL}/api/auth/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload),
+  });
+
+  if (!response.ok) {
+    const { error } = await response.json();
+    return NextResponse.json({ ...error });
+  }
+
+  const { accessToken, refreshToken, ...loginInfo } = await response.json();
+  const res = NextResponse.json({ loginInfo });
+
+  res.cookies.set(ACCESS_TOKEN_STORAGE_KEY, accessToken, {
+    httpOnly: true,
+    secure: true,
+    path: "/",
+    maxAge: ACCESS_TOKEN_MAX_AGE,
+  });
+
+  res.cookies.set(REFRESH_TOKEN_STORAGE_KEY, refreshToken, {
+    httpOnly: true,
+    secure: true,
+    path: "/",
+    maxAge: REFRESH_TOKEN_MAX_AGE,
+  });
+  return res;
+}
