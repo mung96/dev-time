293831b08b36c2c06b8500363ecfb9e8d3e9f4c5
mung96 <mung5079@gmail.com>
2025-12-12 09:59:44 +0900

    feat: 이미지 업로더 퍼블리싱

--- src/pages/member/ui/profile-edit-page.tsx
+++ src/pages/member/ui/profile-edit-page.tsx
@@ -1,3 +1,36 @@
+"use client";
+
+import { ChangeEvent, useRef, useState } from "react";
+
 export const ProfileEditPage = () => {
-  return <div>프로필 수정 페이지</div>;
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleSelectImage = (e: ChangeEvent<HTMLInputElement>) => {};
+  return (
+    <div className="flex w-full items-center justify-center h-full">
+      <div className="flex flex-col gap-2">
+        <label className="text-gray-600 font-medium">프로필 이미지</label>
+        <input
+          type="file"
+          accept=".png, .jpg"
+          ref={fileInputRef}
+          className="hidden"
+          onChange={handleSelectImage}
+        />
+
+        <div className="flex gap-3 items-end">
+          <button
+            onClick={() => fileInputRef.current?.click()}
+            className="border border-primary border-dotted bg-white w-30 h-30 rounded-md"
+          >
+            {/* TODO: icon svg 교체 */}
+            <span className="w-6 h-6 text-primary">+</span>
+          </button>
+          <span className="text-gray-500 font-medium">
+            5MB 미만의 .png, .jpg 파일
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
