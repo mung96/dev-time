dab3d260ff3d89ac19c25590e629f910d3fa218f
mung96 <mung5079@gmail.com>
2025-12-12 09:29:37 +0900

    feat: 프로필 수정 페이지 라우트 설정

---
+++ app/(after-login)/profile/edit/page.tsx
@@ -0,0 +1,5 @@
+import { ProfileEditPage } from "@pages/member/ui/profile-edit-page";
+
+export default function Page() {
+  return <ProfileEditPage />;
+}
---
+++ src/pages/member/ui/profile-edit-page.tsx
@@ -0,0 +1,3 @@
+export const ProfileEditPage = () => {
+  return <div>프로필 수정 페이지</div>;
+};
